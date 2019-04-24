d80702dafb66ced9b1cd8d3694f3db7b3ae44805
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2019-04-24 17:09:13 +0200

    Updated docs

---
+++ docs/build/mycomponent.esm.js
@@ -0,0 +1,1 @@
+import{a as e,b as o}from"./mycomponent-95c6ae8f.js";e().then(e=>{o(null,{resourcesUrl:e})});
---
+++ update-docs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+rm -rf docs
+cp -R www docs
+git add .
+git commit -am "Updated docs"
+git push -u origin nmaster
